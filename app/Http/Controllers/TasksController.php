713af8a8bcd9c9ef675c539d451b0c71afab9a0a
hirohito.hayama <hiro.hellotheworld@gmail.com>
2020-09-28 08:40:59 +0000

    modify_add_column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,8 +48,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+            'status' => 'required|max:10',   
+        ]);
+
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -102,9 +109,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+            'status' => 'required|max:10',   
+        ]);
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;    
         $task->content = $request->content;
         $task->save();
 
